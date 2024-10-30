509af5e73ad09c6825e25a7a129ec0ade523cab1
ibrahimcanaydogan <ibrahim4545erdogan@gmail.com>
2024-10-30 13:22:50 +0300

    Version number increased.

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,10 +7,10 @@ buildscript {
         maven { url = uri("https://maven.google.com") }
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.1.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.48")
-        classpath("org.jetbrains.kotlin:kotlin-serialization:1.9.10")
+        classpath(libs.gradle)
+        classpath(libs.kotlin.gradle.plugin)
+        classpath(libs.hilt.android.gradle.plugin)
+        classpath(libs.kotlin.serialization)
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
